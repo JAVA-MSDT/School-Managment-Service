4ff8c4b3f932a5e9e39a27ad494db6a5d2679ef6
Ahmed Samy <serenitydiver@hotmail.com>
2022-09-04 17:42:20 +0200

    working on main page

--- Front-End/be-best/src/app/public-pages/main-page/main-page.component.ts
+++ Front-End/be-best/src/app/public-pages/main-page/main-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { API_NAME } from 'src/app/app-config/app.conestant';
+import { ROUTER_PATH } from 'src/app/app-config/router-path-const';
+import { Training } from 'src/app/domains/training/training';
+import { ApiService } from 'src/app/service/api/api.service';
 
 @Component({
   selector: 'dc-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.sass']
+  styleUrls: ['./main-page.component.sass'],
 })
 export class MainPageComponent implements OnInit {
+  trainings: Training[] = [];
+  readonly trainingsApi = API_NAME.PUBLIC + ROUTER_PATH.contextPath + API_NAME.TRAININGS;
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.apiService.get<Training[]>(this.trainingsApi).subscribe((training) => {
+      this.trainings = training.slice(0, 3);
+      console.log(this.trainings);
+    });
   }
 
+  goToTrainingPage(trainingId: any) {
+    this.router.navigate([trainingId], { relativeTo: this.route });
+  }
 }
